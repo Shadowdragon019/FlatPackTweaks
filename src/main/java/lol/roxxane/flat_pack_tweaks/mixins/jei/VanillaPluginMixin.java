e67dca199b2c1bbd3a098a76d745d97afba568fb
Creature Girl <wolfboycoolkid@gmail.com>
2025-04-27 13:34:53 -0500

    Updated to JEI 15.20.0.106

--- src/main/java/lol/roxxane/flat_pack_tweaks/mixins/jei/VanillaPluginMixin.java
+++ src/main/java/lol/roxxane/flat_pack_tweaks/mixins/jei/VanillaPluginMixin.java
@@ -3,7 +3,7 @@ package lol.roxxane.flat_pack_tweaks.mixins.jei;
 import mezz.jei.api.recipe.RecipeType;
 import mezz.jei.api.registration.IRecipeCatalystRegistration;
 import mezz.jei.library.plugins.vanilla.VanillaPlugin;
-import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.ItemLike;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
@@ -17,6 +17,6 @@ abstract class VanillaPluginMixin {
 	@Redirect(method = "registerRecipeCatalysts",
 		remap = false,
 		at = @At(value = "INVOKE", ordinal = 0,
-			target = "Lmezz/jei/api/registration/IRecipeCatalystRegistration;addRecipeCatalyst(Lnet/minecraft/world/item/ItemStack;[Lmezz/jei/api/recipe/RecipeType;)V"))
-	private void flat_pack_tweaks$registerRecipeCatalysts_Redirect(IRecipeCatalystRegistration instance, ItemStack ingredient, RecipeType<?>[] recipeTypes) {}
+			target = "Lmezz/jei/api/registration/IRecipeCatalystRegistration;addRecipeCatalysts(Lmezz/jei/api/recipe/RecipeType;[Lnet/minecraft/world/level/ItemLike;)V"))
+	private void flat_pack_tweaks$registerRecipeCatalysts_Redirect(IRecipeCatalystRegistration instance, RecipeType<?> recipeType, ItemLike[] itemLikes) {}
 }
