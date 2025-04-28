a60fab5a061e7f5e429dbd5c3dab746df2ac7c3f
Creature Girl <wolfboycoolkid@gmail.com>
2025-04-27 19:02:23 -0500

    Allow infinite uses of superglue if it is not damage

--- src/main/java/lol/roxxane/flat_pack_tweaks/mixins/create/more_superglues/SuperGlueSelectionHelperMixin.java
+++ src/main/java/lol/roxxane/flat_pack_tweaks/mixins/create/more_superglues/SuperGlueSelectionHelperMixin.java
@@ -3,9 +3,12 @@ package lol.roxxane.flat_pack_tweaks.mixins.create.more_superglues;
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.simibubi.create.content.contraptions.glue.SuperGlueSelectionHelper;
 import lol.roxxane.flat_pack_tweaks.config.FptConfig;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(SuperGlueSelectionHelper.class)
 abstract class SuperGlueSelectionHelperMixin {
@@ -15,5 +18,15 @@ abstract class SuperGlueSelectionHelperMixin {
 			target = "Lnet/minecraft/world/item/ItemStack;getItem()Lnet/minecraft/world/item/Item;"))
 	private static Item fpt$collectGlueFromInventory$ModifyExpressionValue(Item item) {
 		return FptConfig.is_superglue_return_item(item);
+	}
+
+	@Inject(method = "collectGlueFromInventory",
+		remap = false, cancellable = true,
+		at = @At(value = "HEAD"))
+	private static void fpt$collectGlueFromInventory$Inject(Player $, int $1, boolean $2,
+		CallbackInfoReturnable<Boolean> cir
+	) {
+		if (!FptConfig.get_superglue().canBeDepleted())
+			cir.setReturnValue(true);
 	}
 }
