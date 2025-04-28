799fc6af201bfd7dccb2d1a125736cf99db33901
Creature Girl <wolfboycoolkid@gmail.com>
2025-04-28 00:27:03 -0500

    Removed long press on Builders Button keybind

---
+++ src/main/java/lol/roxxane/flat_pack_tweaks/mixins/remove_builder_mode/BuildersButtonMixin.java
@@ -0,0 +1,17 @@
+package lol.roxxane.flat_pack_tweaks.mixins.remove_builder_mode;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import snownee.kiwi.customization.builder.BuildersButton;
+
+@Mixin(BuildersButton.class)
+abstract class BuildersButtonMixin {
+	@Inject(method = "onLongPress",
+		remap = false, cancellable = true,
+		at = @At("HEAD"))
+	private static void fpt$onLongPress$Inject(CallbackInfoReturnable<Boolean> cir) {
+		cir.setReturnValue(false);
+	}
+}
