79403de6e852f4300957bb3f6afdab370038abd5
Creature Girl <wolfboycoolkid@gmail.com>
2025-05-25 20:07:54 -0500

    Lube is now shown in the sawing category

--- src/main/java/lol/roxxane/flat_pack_tweaks/mixins/create/lubeable_mechanical_saw/Jei.java
+++ src/main/java/lol/roxxane/flat_pack_tweaks/mixins/create/lubeable_mechanical_saw/Jei.java
@@ -2,16 +2,24 @@ package lol.roxxane.flat_pack_tweaks.mixins.create.lubeable_mechanical_saw;
 
 import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
 import com.llamalad7.mixinextras.sugar.Local;
+import com.simibubi.create.compat.jei.category.CreateRecipeCategory;
 import com.simibubi.create.compat.jei.category.SawingCategory;
 import com.simibubi.create.compat.jei.category.animations.AnimatedSaw;
 import com.simibubi.create.content.kinetics.saw.CuttingRecipe;
 import lol.roxxane.flat_pack_tweaks.LubeHelper;
 import lol.roxxane.flat_pack_tweaks.mixin_external_classes.create.lubeable_mechanical_saw.AnimatedLubedSaw;
+import lol.roxxane.flat_pack_tweaks.tags.FptItemTags;
+import mezz.jei.api.gui.builder.IRecipeLayoutBuilder;
+import mezz.jei.api.recipe.IFocusGroup;
+import mezz.jei.api.recipe.RecipeIngredientRole;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.crafting.Ingredient;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(value = SawingCategory.class, remap = false)
 abstract class Jei {
@@ -30,5 +38,15 @@ abstract class Jei {
 				Component.translatable("gui.flat_pack_tweaks.category.sawing.dry_2"), 0, 60, 0xFFFFFF);
 		}
 		return true;
+	}
+
+	@Inject(method = "setRecipe(Lmezz/jei/api/gui/builder/IRecipeLayoutBuilder;Lcom/simibubi/create/content/kinetics/saw/CuttingRecipe;Lmezz/jei/api/recipe/IFocusGroup;)V",
+		at = @At("HEAD"))
+	private void fpt$Inject$setRecipe(IRecipeLayoutBuilder builder, CuttingRecipe recipe, IFocusGroup $,
+		CallbackInfo $1) {
+		if (LubeHelper.requires_lube(recipe))
+			builder.addSlot(RecipeIngredientRole.INPUT, 32, 48)
+				.setBackground(CreateRecipeCategory.getRenderedSlot(), -1, -1)
+				.addIngredients(Ingredient.of(FptItemTags.LUBE));
 	}
 }
