92cfc4951f1d6decae38ec68d77602d0b782a4d8
Creature Girl <wolfboycoolkid@gmail.com>
2025-05-29 07:17:37 -0500

    Removed an old TODO

--- src/main/java/lol/roxxane/flat_pack_tweaks/Fpt.java
+++ src/main/java/lol/roxxane/flat_pack_tweaks/Fpt.java
@@ -17,7 +17,6 @@ import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
-// TODO: Make deployers able to lube mechanical saws
 @Mod(Fpt.ID)
 public final class Fpt {
 	public static final String ID = "flat_pack_tweaks";
